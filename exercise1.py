20adc2fa2a15f122d5093da2bbfc9625bb2ed772
Momomelo <y.kei1ro@gmail.com>
2015-09-24 15:01:49 -0400

    Revert "Added period to line 6"
    This reverts commit 66ca87943111e5c117578b7407c4e3e752fd195d.

--- exercise1.py
+++ exercise1.py
@@ -3,7 +3,7 @@
 """ Assignment 1, Exercise 1, INF1340, Fall, 2014. Grade to gpa conversion
 
 This module prints the amount of money that Lakshmi has remaining
-after the stock transactions.
+after the stock transactions
 
 """
 
